8041476225e30c91e168a64794098d3a76a84bb6
krishnapriya11300 <90126423+krishnapriya11300@users.noreply.github.com>
2021-09-05 21:39:01 -0700

    Add files via upload

---
+++ test_test_primes.py
@@ -0,0 +1,20 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: E:/python/solution.py ========================
+Largest element is: 4
+Largest number is: 4
+Second Largest number is :  3
+[85, 41]
+True
+Currency Count -> 
+500  :  1
+200  :  1
+100  :  1
+50  :  1
+10  :  1
+5  :  1
+1  :  3
+Enter file name: java
+ file is extremely large so it cannot be read:error
+>>> 
